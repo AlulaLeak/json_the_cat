d380f202e3cac61790fe8662c7faa8e8212b4275
AlulaLeak <alula.leakemariam94@gmail.com>
2021-12-03 00:07:16 +0000

    breed desc fetch

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,2 +1,19 @@
 const request = require('request');
 
+let catName = (process.argv).pop();
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${catName}`, function (error, response, body) {
+
+  if (error) {
+    console.error('error:', error);
+  }
+
+  body = JSON.parse(body);
+  const breed = body[0];
+
+  if (breed) {
+    console.log(breed.description);
+  } else {
+    console.log(`failed to find breed ${catName}`);
+  }
+});
